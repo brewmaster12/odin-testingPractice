e4fe304c978e1b61ab7610706905650536e0e95f
Sean <seanywg@gmail.com>
2026-07-05 20:39:17 +0100

    add capitalise test and function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function capitalise(string) {
+    return string.charAt(0).toUpperCase()
+    + string.slice(1);
+}
 
+export {
+    capitalise
+}
--- script.test.js
+++ script.test.js
@@ -1,1 +1,5 @@
+import { capitalise } from './script';
 
+test("Capitalises first letter", () => {
+    expect(capitalise("samantha")).toBe("Samantha");
+});
